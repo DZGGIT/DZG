6a2ef451bf9a8b147e1b5775e6f6567fe08268ec
DZGGIT <2321271369@qq.com>
2015-11-20 18:09:57 +0800

    初始化

---
+++ textView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  textView
+//
+//  Created by shenen on 15/11/17.
+//  Copyright © 2015年 cntysoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSError *error;
+    NSString *text=[NSString stringWithContentsOfFile:[[NSBundle mainBundle]pathForResource:@"1.txt" ofType:@"txt"] encoding: NSUTF16StringEncoding error:&error];
+    NSLog(@"%@",text);
+    // Do any additional setup after loading the view, typically from a nib.
+}
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ textView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  textView
+//
+//  Created by shenen on 15/11/17.
+//  Copyright © 2015年 cntysoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
